0c100515bbc1631401cc1ddce67d7da09a9257d0
Aman <AmanPraveen70@gmail.com>
2021-01-04 09:33:34 +0000

    instead of using for loop to iterate of the same script - a function was put in place to replace copies of existing codee

--- pyorganiser.py
+++ pyorganiser.py
@@ -6,28 +6,18 @@ os.chdir('\\Users\\amanp\\Downloads')
 
 download_files = os.listdir()
 
-for files in os.listdir():
-    if files.endswith(".jpg") or files.endswith(".png") or files.endswith(".mp4") or files.endswith(".gif") or files.endswith(".jpeg"):
-        pof = os.path.join(os.getcwd(),files)
-        shutil.move(pof,"C:\\Users\\amanp\\Downloads\\PyOrganise\\Picture Files")
-        print ("Moved: " + files)
-        print ()
-        
-    elif files.endswith(".exe") or files.endswith(".zip"):
-        pof2 = os.path.join(os.getcwd(),files)
-        shutil.move(pof2,"C:\\Users\\amanp\\Downloads\\PyOrganise\\.exe and zip Files")
-        print ("Moved: " + files)
-        print ()
 
-    elif files.endswith(".mp4") or files.endswith(".mov"):
-        pof3 = os.path.join(os.getcwd(),files)
-        shutil.move(pof3,"C:\\Users\\amanp\\Downloads\\PyOrganise\\Video files")
-        print ("Moved: " + files)
-        print ()
+def clean(filetypes, path):
+    for files in download_files:
+        for f in filetypes:
+            if files.endswith(f):
+                pof = os.path.join(os.getcwd(),files)
+                shutil.copy(pof, path)
+                os.remove(files)
+                print ("Moved: " + files)
+                print ()
 
-
-    elif files.endswith(".pdf") or files.endswith(".docx") or files.endswith(".doc") or files.endswith(".ppt") or files.endswith(".pptx") or files.endswith(".xls") or files.endswith(".xlsx") or files.endswith.(".csv"):
-        pof3 = os.path.join(os.getcwd(),files)
-        shutil.move(pof3,"C:\\Users\\amanp\\Downloads\\PyOrganise\\Microsoft Office files")
-        print ("Moved: " + files)
-        print ()
+clean([".png","jpg", ".jpeg"], "C:\\Users\\amanp\\Downloads\\PyOrganise\\Picture files")
+clean([".mp4", ".mov"], "C:\\Users\\amanp\\Downloads\\PyOrganise\\Picture files")
+clean([".exe",".zip","tar.gz"], "C:\\Users\\amanp\\Downloads\\PyOrganise\\Picture files")
+clean([".pdf",".doc",".docx",".ppt",".pptx","xls","xlsx",".csv"], "C:\\Users\\amanp\\Downloads\\PyOrganise\\Picture files")
